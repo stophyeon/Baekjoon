acad1a516e475260ff34223d3137f1aba3b8bb62
jihyeon jeong <122667296+stophyeon@users.noreply.github.com>
2024-08-26 12:33:42 +0900

    [level 1] Title: 삼총사, Time: 0.33 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,14 +1,31 @@
+import java.util.*;
 class Solution {
+    int[] numbers;
+    
+    int answer = 0;
     public int solution(int[] number) {
-        int answer = 0;
-        for(int i=0; i<number.length-2; i++){
-            for(int j=i+1; j<number.length-1; j++){
-                for(int k=j+1; k<number.length; k++){
-                    int sum = number[i]+number[j]+number[k];
-                    if(sum==0){answer++;}
-                }
+        numbers=number;
+        boolean[] visited = new boolean[number.length];
+        dfs(0,0,1,visited);
+        
+        return answer;
+    }
+    public void dfs(int i, int sum,int depth, boolean[] visited){
+        
+        if(depth==4){
+            if(sum==0){
+                answer++;
+            }
+            return;
+        }
+        
+        for(int j=0; j<numbers.length; j++){
+            if(!visited[j]){
+                visited[j]=true;
+                boolean[] visit = Arrays.copyOf(visited,visited.length);
+                dfs(j,sum+numbers[j],depth+1,visit);
             }
         }
-        return answer;
+        
     }
 }
