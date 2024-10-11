fadd7e86369a55f839575e38fbc6073f1b5669e0
jihyeon jeong <122667296+stophyeon@users.noreply.github.com>
2024-10-11 15:20:11 +0900

    [level 3] Title: 양과 늑대, Time: 15.26 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,41 +1,50 @@
+import java.util.*;
 class Solution {
-
-    int[] gInfo;
-    int[][] gEdges;
-    int maxSheepCnt = 0;
-
+    int answer = 0;
+    HashMap<Integer,List<Integer>> map = new HashMap<>();
+    int[] type;
+    
     public int solution(int[] info, int[][] edges) {
-        gInfo = info;
-        gEdges = edges;
-        boolean[] initVisited = new boolean[info.length];
-        dfs(0, initVisited, 0, 0);
-
-        return maxSheepCnt;
-    }
-
-    public void dfs(int idx, boolean[] visited, int sheepCnt, int wolfCnt) {
-        visited[idx] = true;
-        if (gInfo[idx] == 0) {
-            sheepCnt++;
-            if (sheepCnt > maxSheepCnt) {
-                maxSheepCnt = sheepCnt;
+        type=info;
+        for(int[] edge : edges){
+            if(map.containsKey(edge[0])){
+                map.get(edge[0]).add(edge[1]);
             }
-        } else {
-            wolfCnt++;
-        }
-
-        if (sheepCnt <= wolfCnt) {
-            return;
-        }
-
-        for (int[] edge : gEdges) {
-            if (visited[edge[0]] && !visited[edge[1]]) {
-            	boolean[] nextVisited = new boolean[visited.length];
-            	for (int i = 0; i < visited.length; i++) {
-                	nextVisited[i] = visited[i];
-            	}
-                dfs(edge[1], nextVisited, sheepCnt, wolfCnt);
+            else{
+                List<Integer> l = new ArrayList<>();
+                l.add(edge[1]);
+                map.put(edge[0],l);
             }
         }
+        
+        boolean[] visited = new boolean[info.length];
+        visited[0]=true;
+        
+        List<Integer> path = new ArrayList<>();
+        for(int i=0; i<map.get(0).size(); i++){
+            path.add(map.get(0).get(i));
+        }
+        
+        dfs(0,path,visited,1,0);
+        return answer;
     }
+    public void dfs(int n, List<Integer> path ,boolean[] visited,int s, int w){
+        if(w>=s) return;
+        answer = Math.max(answer, s);
+        for(int num : path){
+            if(visited[num]) continue;
+            visited[num]=true;
+            List<Integer> p = new ArrayList<>(path);
+            if(map.containsKey(num)){
+                for(int i=0; i<map.get(num).size(); i++){
+                    p.add(map.get(num).get(i));
+                }
+            } 
+            
+            if(type[num]==0) dfs(num, p,visited,s+1,w);
+            if(type[num]==1) dfs(num, p,visited,s,w+1);
+            visited[num]=false;
+        }
+    }
+    
 }
