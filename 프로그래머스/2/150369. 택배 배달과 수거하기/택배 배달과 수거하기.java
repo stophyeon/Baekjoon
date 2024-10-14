579f93556aa0d7ace3177d1e5b48563dc8a7b7cb
jihyeon jeong <122667296+stophyeon@users.noreply.github.com>
2024-10-14 21:57:16 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 7562.86 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,19 +1,64 @@
 class Solution {
+    int[] deliverie;
+    int[] pickup;
+    int c;
+    long answer = 0;
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long answer = 0;
-        int d = 0;
-        int p = 0;
-        for(int i=n-1; i>=0; i--){
-            d -= deliveries[i];
-            p -= pickups[i];
-            
-            while(d < 0 || p < 0){
-                d += cap;
-                p += cap;
-                answer += (i+1)*2;
+        deliverie=deliveries;
+        pickup=pickups;
+        c=cap;
+        int index=n-1;
+        while(true){
+            int n1=0;
+            int n2=0;
+            index=truck(index);
+            for(int j=0; j<n; j++){
+                if(deliverie[j]!=0 || pickup[j]!=0) break;
+                if(deliverie[j]==0) n1++;
+                if(pickup[j]==0) n2++;
+            }
+            if(n1==n&&n2==n) break;
+        }
+        
+        return answer*2;
+    }
+    
+    public int truck(int idx){
+        int d=0;
+        int p=0;
+        int md=0;
+        int mp=0;
+        int max=-1;
+        for(int i = idx; i>=0; i--){
+            if(deliverie[i]!=0){
+                max=Math.max(max,i);
+            }
+            if(d+deliverie[i]>c) {
+                deliverie[i]-=c-d;
+                md=i;
+                break;
+            }
+            else{
+                d+=deliverie[i];
+                deliverie[i]=0;
+            }   
+        }
+        for(int i=idx; i>=0; i--){
+            if(pickup[i]!=0){
+                max=Math.max(max,i);
+            }
+            if(p+pickup[i]>c) {
+                pickup[i]-=c-p;
+                mp=i;
+                break;  
+            }
+            else{
+               p+=pickup[i];
+               pickup[i]=0; 
             }
         }
         
-        return answer;
+        answer+=max+1;
+        return Math.max(md,mp);
     }
 }
