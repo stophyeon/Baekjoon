47534663e492afc55cdaac88835291c6666cb947
jihyeon jeong <122667296+stophyeon@users.noreply.github.com>
2024-12-13 18:56:54 +0900

    [level 2] Title: 과제 진행하기, Time: 26.68 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -1,120 +1,69 @@
 import java.util.*;
-
 class Solution {
-    
-    static class Task {
-        private String name;
-        private int start;
-        private int playtime;
-        
-        public Task(String name, int start, int playtime) {
-            this.name = name;
-            this.start = start;
-            this.playtime = playtime;
-        }
-        
-        public Task(String name, int playtime) {
-            this.name = name;
-            this.playtime = playtime;
+    public class Plan{
+        String name;
+        int time;
+        public Plan(String name, int time){
+            this.name=name;
+            this.time=time;
         }
     }
-    
-    public List<String> solution(String[][] plans) {
-    	// 정답을 저장할 리스트
-        List<String> answer = new ArrayList<>();
+    HashMap<Integer,Plan> map = new HashMap<>();
+    List<Integer> next = new ArrayList<>();
+    Stack<Plan> st = new Stack<>();
+    public String[] solution(String[][] plans) {
         
-        // 해야할 과제들을 시작시간 순으로 저장
-        PriorityQueue<Task> pq = new PriorityQueue<>(
-            (o1, o2) -> (o1.start - o2.start)
-        );
-        
-        for(int i = 0; i < plans.length; i++) {
-            String name = plans[i][0];
-            
-            String[] str = plans[i][1].split(":");
-            int h = Integer.parseInt(str[0]);
-            int m = Integer.parseInt(str[1]);
-            int start = (h * 60) + m;
-            
-            int time = Integer.parseInt(plans[i][2]);
-            
-            pq.add(new Task(name, start, time));
+        String[] answer = new String[plans.length];
+        String ans="";
+        for(int i=0; i<plans.length; i++){
+            String[] time = plans[i][1].split(":");
+            int h = Integer.parseInt(time[0])*60;
+            int m = Integer.parseInt(time[1]);
+            next.add(h+m);
+            map.put(h+m,new Plan(plans[i][0],Integer.parseInt(plans[i][2])));
         }
+        Collections.sort(next,(t1,t2)->t1-t2);
         
-        // 잠시 멈춘 과제를 저장
-        Stack<Task> remainingTasks = new Stack<>();
-        
-        while(!pq.isEmpty()) {
-            Task currentTask = pq.poll();
-            
-            String curName = currentTask.name;
-            int curStart = currentTask.start;
-            int curPlaytime = currentTask.playtime;
-            
-            // 현재 시각
-            int currentTime = curStart;
-            
-            // 새로운 과제가 남아있는 경우(진행중이던 과제 제외)
-            if(!pq.isEmpty()) {
-                Task nextTask = pq.peek();
-                
-                // 지금 과제를 끝내고도 다음 과제 시작까지 시간이 남는 경우
-                if(currentTime + curPlaytime < nextTask.start) {
-                    answer.add(curName);
-                    currentTime += curPlaytime;
-                    
-                    // 잠시 멈춘 과제가 있는 경우, 남는 시간동안 멈췄던 과제 해결
-                    while(!remainingTasks.isEmpty()) {
-                        Task rem = remainingTasks.pop();
-                        
-                        // 다음 새로운 과제 시작전까지 다 끝낼수 있는 경우
-                        if(currentTime + rem.playtime <= nextTask.start) {
-                            currentTime += rem.playtime;
-                            answer.add(rem.name);
-                            continue;
-                        } 
-                        // 다음 새로운 과제 시작전까지 못 끝내는 경우
-                        else {
-                            int t = rem.playtime - (nextTask.start - currentTime);
-                            // 추가로 한 시간만 빼서 멈춘 과제 목록에 다시 추가
-                            remainingTasks.push(new Task(rem.name, t));
+        for(int i=0; i<next.size()-1; i++){
+            int t=next.get(i);
+            Plan p=map.get(next.get(i));
+            if(p.time+next.get(i)==next.get(i+1)){
+                ans+=p.name+",";
+                continue;
+            }
+            else if(p.time+next.get(i)<next.get(i+1)){
+                ans+=p.name+",";
+                t+=p.time;
+                if(!st.isEmpty()){
+                    while(t<next.get(i+1)){
+                        Plan rest = st.pop();
+                        if(t+rest.time>next.get(i+1)){
+                            rest.time-=next.get(i+1)-t;
+                            st.push(rest);
                             break;
                         }
+                        t+=rest.time;
+                        ans+=rest.name+",";
+                        if(st.isEmpty()) break;
                     }
                 }
-                // 지금 과제 끝내면 새로운 과제 시작할 시간인 경우
-                else if(curStart + curPlaytime == nextTask.start) {
-                    answer.add(curName);
-                    continue;
-                }
-                // 새로운 과제 시작전까지 지금 과제를 못 끝내는 경우
-                else {
-                    int t = (nextTask.start - currentTime);
-                    remainingTasks.push(new Task(curName, curPlaytime - t));
-                }
-                
             }
-            
-            // 더 이상 남아있는 새로운 과제가 없는 경우
-            else {
-                // 남아있는 과제(잠시 멈춘 과제)도 없는 경우
-                if(remainingTasks.isEmpty()) {
-                    currentTime += curPlaytime;
-                    answer.add(curName);
-                }
-                // 남아있는 과제는 있는 경우
-                else {
-                    answer.add(curName); // 새로운 과제부터 먼저 해결
-                    
-                    // 남아있는 과제들을 정해진 순서대로 끝내면 됨
-                    while(!remainingTasks.isEmpty()) {
-                        Task rem = remainingTasks.pop();
-                        answer.add(rem.name);
-                    }
-                }
+            else{
+                p.time-=next.get(i+1)-next.get(i);
+                st.push(p);
             }
         }
-        
+        ans+=map.get(next.get(next.size()-1)).name+",";
+        // answer[idx]=map.get(next.get(next.size()-1)).name;
+        // idx++;
+        while(!st.isEmpty()){
+            Plan p = st.pop();
+            ans+=p.name+",";
+            // answer[idx]=p.name;
+            // idx++;
+        }
+        System.out.println(ans);
+        answer=ans.split(",");
         return answer;
     }
 }
