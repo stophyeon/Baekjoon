72d1a71a4798c6974a6165e707a3a26642d9d6dd
jihyeon jeong <122667296+stophyeon@users.noreply.github.com>
2024-12-13 15:25:41 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 56.54 ms, Memory: 214 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,41 +1,54 @@
 class Solution {
     int[][] map;
+    int[][] prefix;
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
-        map= new int[board.length+1][board[0].length+1];
+        map=board;
+        int n=board.length;
+        int m=board[0].length;
+        prefix=new int[n+1][m+1];
         for(int[] s : skill){
-            //공격
             if(s[0]==1){
-                map[s[1]][s[2]]+=-1*s[5];
-                map[s[1]][s[4]+1]+=s[5];
-                map[s[3]+1][s[2]]+=s[5];
-                map[s[3]+1][s[4]+1]+=-1*s[5];
+                attack(s[1],s[2],s[3],s[4],s[5]*(-1));
             }
-            //회복
             else{
-                map[s[1]][s[2]]+=s[5];
-                map[s[1]][s[4]+1]+=-1*s[5];
-                map[s[3]+1][s[2]]+=-1*s[5];
-                map[s[3]+1][s[4]+1]+=s[5];
+                hill(s[1],s[2],s[3],s[4],s[5]);
             }
         }
-        for(int j=0; j<board[0].length+1; j++){
-            for(int i=1; i<board.length+1; i++){
-                map[i][j]+=map[i-1][j];
+        for(int i=0; i<m; i++){
+            for(int j=1; j<n; j++){
+                prefix[j][i]+=prefix[j-1][i];
             }
         }
-        for(int i=0; i<board.length+1; i++){
-            for(int j=1; j<board[0].length+1; j++){
-                map[i][j]+=map[i][j-1];
+        for(int i=0; i<n; i++){
+            for(int j=1; j<m; j++){
+                prefix[i][j]+=prefix[i][j-1];
             }
         }
-        
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++){
-                board[i][j]+=map[i][j];
-                if(board[i][j]>0) answer++;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                map[i][j]+=prefix[i][j];
+                if(map[i][j]>0) answer++;
             }
         }
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         System.out.print(prefix[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
         return answer;
+    }
+    public void attack(int r1, int c1, int r2, int c2, int d){
+        prefix[r1][c1]+=d;
+        prefix[r1][c2+1]-=d;
+        prefix[r2+1][c1]-=d;
+        prefix[r2+1][c2+1]+=d;
+    }
+    public void hill(int r1, int c1, int r2, int c2, int d){
+        prefix[r1][c1]+=d;
+        prefix[r1][c2+1]-=d;
+        prefix[r2+1][c1]-=d;
+        prefix[r2+1][c2+1]+=d;
     }
 }
