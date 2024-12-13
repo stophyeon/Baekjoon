1288454f11b6c4b5a15ca8862dd28fe83e703e0b
jihyeon jeong <122667296+stophyeon@users.noreply.github.com>
2024-12-13 19:57:59 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.26 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,70 +1,85 @@
 class Solution {
     public int solution(String[] board) {
-        int Ocnt = 0;
-        int Xcnt = 0;
-        //O와 X의 개수를 센다.
-        for (int i = 0; i < 3; i++) {
-            Ocnt += countChar(board[i], 'O');
-            Xcnt += countChar(board[i], 'X');
-        }
-
-        //'X'가 'O'보다 많으면 규칙 위반
-        if (Xcnt > Ocnt) {
-            return 0;
-        }
-
-        //'O'가 'X'보다 2개이상 많으면 규칙 위반
-        if (Ocnt > Xcnt + 1) {
-            return 0;
-        }
-
-        //O가 완성 되었을 때 X가 O의 개수와 같으면 규칙 위반
-        if (hasWin(board, 'O')) {
-            if (Ocnt == Xcnt) {
-                return 0;
+        int answer = 0;
+        int[][] map = new int[3][3];
+        int o=0;
+        int x=0;
+        boolean xbingo=false;
+        boolean obingo=false;
+        int r=0;
+        int c=0;
+        for(String b : board){
+            String[] ox = b.split("");
+            c=0;
+            for(String t : ox){
+                if(t.equals("O")) {o++;map[r][c]=1;}
+                if(t.equals("X")){x++;map[r][c]=2;}
+                c++;
             }
+            r++;
         }
-        //X가 완성 되었을 때 O가 X보다 1개 많으면 규칙 위반
-        if (hasWin(board, 'X')) {
-            if (Ocnt == Xcnt + 1) {
-                return 0;
+        int on=0;
+        int xn=0;
+        //가로
+        for(int i=0; i<3; i++){
+            on=0;
+            xn=0;
+            for(int j=0; j<3; j++){
+                if(map[i][j]==0) {break;}
+                if(map[i][j]==1) {on++;}
+                if(map[i][j]==2) {xn++;}
             }
+            if(on==3) obingo=true;
+            if(xn==3) {xbingo=true;}
         }
+        //세로
+        on=0;
+        xn=0;
+        
+        for(int i=0; i<3; i++){
+            on=0;
+            xn=0;
+            for(int j=0; j<3; j++){
+                if(map[j][i]==0) {break;}
+                if(map[j][i]==1) {on++;}
+                if(map[j][i]==2) {xn++;}
+            }
+            if(on==3) obingo=true;
+            if(xn==3) {xbingo=true;}
+        }
+        //대각선
+        on=0;
+        xn=0;
+        for(int i=0; i<3; i++){
+            if(map[i][i]==0) {break;}
+            if(map[i][i]==1) {on++;}
+            if(map[i][i]==2) {xn++;}
+        }
+        if(on==3) obingo=true;
+        if(xn==3) {xbingo=true;}
+        on=0;
+        xn=0;
+        for(int i=0; i<3; i++){
+            if(map[i][2-i]==0) {break;}
+            if(map[i][2-i]==1) {on++;}
+            if(map[i][2-i]==2) {xn++;}
+        }
+        if(on==3) obingo=true;
+        if(xn==3) {xbingo=true;}
+        
+         // System.out.println(obingo);
+         // System.out.println(xbingo);
+         // System.out.println(o);
+         // System.out.println(x);
+        if(obingo&&xbingo) return 0;
+        if(o<x) return 0;
+        if(obingo) {
+            if(o!=x+1) return 0;
+        }
+        if(xbingo){
+            if(x!=o) return 0;
+        } 
+        if(o-x>1) return 0;
         return 1;
-    }
-
-    private static int countChar(String str, char ch) {
-        return str.length() - str.replace(String.valueOf(ch), "").length();
-    }
-
-    private static boolean hasWin(String[] board, char ch) {
-        //가로 검사
-        for (int i = 0; i < 3; i++) {
-            if (board[i].charAt(0) == ch
-                    && board[i].charAt(1) == ch
-                    && board[i].charAt(2) == ch) {
-                return true;
-            }
-        }
-        //세로 검사
-        for (int i = 0; i < 3; i++) {
-            if (board[0].charAt(i) == ch
-                    && board[1].charAt(i) == ch
-                    && board[2].charAt(i) == ch) {
-                return true;
-            }
-        }
-        //대각선 검사
-        if (board[0].charAt(0) == ch
-                && board[1].charAt(1) == ch
-                && board[2].charAt(2) == ch) {
-            return true;
-        }
-        if (board[0].charAt(2) == ch
-                && board[1].charAt(1) == ch
-                && board[2].charAt(0) == ch) {
-            return true;
-        }
-        return false;
     }
 }
