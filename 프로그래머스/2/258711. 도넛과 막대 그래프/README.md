7449f128ae09e7a1f77e90ee0857b786535e93da
jihyeon jeong <122667296+stophyeon@users.noreply.github.com>
2025-10-09 17:45:20 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 756.47 ms, Memory: 366 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/README.md
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/README.md
@@ -1,10 +1,10 @@
 # [level 2] 도넛과 막대 그래프 - 258711 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258711#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258711) 
 
 ### 성능 요약
 
-메모리: 154 MB, 시간: 47.91 ms
+메모리: 366 MB, 시간: 756.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 29일 18:03:52
+2025년 10월 09일 17:45:17
 
 ### 문제 설명
 
--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,34 +1,69 @@
 import java.util.*;
+
 class Solution {
+    
+    HashMap<Integer, Integer> in = new HashMap<>();
+    HashMap<Integer, Integer> out = new HashMap<>();
+    HashMap<Integer, List<Integer>> map = new HashMap<>();
+    int[] answer = new int[4];
+    
     public int[] solution(int[][] edges) {
-        int[] answer = new int[4];
-        HashMap<Integer,int[]> vertex = new HashMap<>();
-        int max=0;
-        int num=0;
-        for(int[] edge :edges){
-            if(max<edge[0]) max=edge[0];
-            if(max<edge[1]) max=edge[1];
-        }
         
-        int[] in = new int[max+1];
-        int[] out = new int[max+1];
-        
-        for(int[] edge :edges){
-            in[edge[1]]+=1;
-            out[edge[0]]+=1;
+        //무관한 정점 찾기
+        for(int[] edge : edges){
+            if(out.containsKey(edge[0])){
+                out.put(edge[0],out.get(edge[0])+1);
+            }
+            else if(!out.containsKey(edge[0])){
+                out.put(edge[0],1);
+            }
+            if(in.containsKey(edge[1])){
+                in.put(edge[1],in.get(edge[1])+1);
+            }
+            else if(!in.containsKey(edge[1])){
+                in.put(edge[1],1);
+            }
+            if(map.containsKey(edge[0])){
+                map.get(edge[0]).add(edge[1]);
+            } else if(!map.containsKey(edge[0])){
+                List<Integer> list = new ArrayList<>();
+                list.add(edge[1]);
+                map.put(edge[0],list);
+            }
         }
-        for(int i=1; i<=max; i++){
-            if(in[i]==0&&out[i]>=2) {answer[0]=i;num=out[i];break;}
+        for(int k : out.keySet()){
+            if(!in.containsKey(k)&&out.get(k)>=2) answer[0]=k;
         }
-        
-        for(int i=1; i<=max; i++){
-            //막대
-            if(in[i]>=1&&out[i]==0) answer[2]+=1;
-            //8자
-            if(in[i]>=2&&out[i]==2) answer[3]+=1;
+        //무관한 정점에서 들어오는 간선은 개수에서 제외
+        for(int k : map.get(answer[0])){
+            in.put(k,in.get(k)-1);
+        }
+        // for(int k : map.keySet()){
+        //     System.out.print(k+" - ");
+        //     for(int v : map.get(k)){
+        //         System.out.print(v+",");
+        //     }
+        //     System.out.println();
+        // }
+        int num2=0;
+        int num3=0;
+        //들어오는 간선만 있고 나가는 간선이 없는 정점 - 막대
+        //나가는 간선2개, 들어오는 간선 2개 - 8자
+        for(int k : in.keySet()){
+            if(!out.containsKey(k)) num2++;
+            else{
                 
+                if(in.get(k)==2&&out.get(k)==2) num3++;
+            }
         }
-        answer[1]=num-answer[2]-answer[3];
+        
+        answer[2]=num2;
+        answer[3]=num3;
+        //나머지가 도넛
+        answer[1]=map.get(answer[0]).size()-(num2+num3);
+        
         return answer;
     }
+    
+    
 }
