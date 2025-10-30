90123edecc958124493f3325d7e606d7ac73a8fc
jihyeon jeong <122667296+stophyeon@users.noreply.github.com>
2025-10-30 21:28:13 +0900

    [level 2] Title: 완전범죄, Time: 43.08 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/README.md
+++ 프로그래머스/2/389480. 완전범죄/README.md
@@ -1,10 +1,10 @@
 # [level 2] 완전범죄 - 389480 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389480) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389480#qna) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 5.62 ms
+메모리: 83.1 MB, 시간: 43.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 30일 21:16:19
+2025년 10월 30일 21:28:11
 
 ### 문제 설명
 
--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,52 +1,37 @@
-import java.util.*;
-
-class Solution {
-    public int solution(int[][] info, int n, int m) {
-        int A = n; // A의 '미만' 용량
-        int B = m; // B의 '미만' 용량
-        int cnt = info.length;
-
-        // dp[a][b] = A가 a, B가 b 사용한 상태가 가능
-        boolean[][] dp = new boolean[A][B];
-        dp[0][0] = true;
-
-        for (int i = 0; i < cnt; i++) {
-            int wa = info[i][0]; // A에 넣을 때 증가량
-            int wb = info[i][1]; // B에 넣을 때 증가량
-
-            boolean[][] next = new boolean[A][B];
-
-            for (int a = 0; a < A; a++) {
-                for (int b = 0; b < B; b++) {
-                    if (!dp[a][b]) continue;
-
-                    // A에 넣는 경우 (미만 조건 유지)
-                    int na = a + wa;
-                    if (na < A) {
-                        next[na][b] = true;
-                    }
-
-                    // B에 넣는 경우 (미만 조건 유지)
-                    int nb = b + wb;
-                    if (nb < B) {
-                        next[a][nb] = true;
-                    }
-                }
-            }
-            dp = next;
-        }
-
-        // 가능한 상태 중 A 사용량 최소값
-        int ans = -1;
-        outer:
-        for (int a = 0; a < A; a++) {
-            for (int b = 0; b < B; b++) {
-                if (dp[a][b]) {
-                    ans = a;
-                    break outer;
-                }
-            }
-        }
-        return ans;
-    }
+import java.util.*; 
+class Solution { 
+    int min = 0; 
+    int cnt = 0; 
+    int[][] map; 
+    int A=0; 
+    int B=0; 
+    HashMap<String,Integer> visit = new HashMap<>();
+    public int solution(int[][] info, int n, int m) { 
+        int answer = -1; 
+        A=n; 
+        B=m; 
+        min = n; 
+        //물건의 총개수 
+        cnt = info.length; 
+        map=info; 
+        
+        dfs(0,0,0); 
+        if(min!=n) answer = min; 
+        return answer; 
+    } 
+    
+    public void dfs(int a, int b, int num){ 
+        //모든 물건을 훔쳤을 때 
+        if(num==cnt){ 
+            if(min > a) min = a; 
+            return; 
+        } 
+        String k = a+","+b+","+num;
+        if(visit.containsKey(k)) return;
+        //n을 넘지않고 A가 훔쳤을 때 
+        if(a+map[num][0]<A) dfs(a+map[num][0],b,num+1); 
+        //m을 넘지않고 B가 훔쳤을 때 
+        if(b+map[num][1]<B) dfs(a,b+map[num][1],num+1); 
+        visit.put(k,0); 
+    } 
 }
