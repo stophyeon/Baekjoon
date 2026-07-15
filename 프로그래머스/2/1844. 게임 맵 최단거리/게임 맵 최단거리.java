656a0c769240d075debc7740ea6624d7e83b3101
jihyeon jeong <122667296+stophyeon@users.noreply.github.com>
2026-07-15 16:19:47 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.73 ms, Memory: 57.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,45 +1,47 @@
 import java.util.*;
-class Solution {
-    class Node{
-        int x;
-        int y;
-        int d;
-        public Node(int x,int y, int d){
-            this.x=x;
-            this.y=y;
-            this.d=d;
-        }
+public class Node{
+    int r;
+    int c;
+    int dis;
+    public Node(int r, int c, int dis){
+        this.r=r;
+        this.c=c;
+        this.dis=dis;
     }
-    
+}
+class Solution {
+    int[] dr = {-1,0,0,1};
+    int[] dc = {0,1,-1,0};
+    int n,m;
     public int solution(int[][] maps) {
-        int[] dx = {1,-1,0,0};
-        int[] dy = {0,0,1,-1};
-        int answer = 10000;
-        int desy=maps.length;
-        int desx=maps[0].length;
-        boolean[][] visited = new boolean[desy][desx];
-        Queue<Node> q= new LinkedList<>();
-        
-        q.add(new Node(0,0,1));
-        
-        
-        while(!q.isEmpty()){
-            Node n = q.poll();
-            //System.out.println(n.x+" "+n.y+" "+"거리:"+n.d);
-            if(n.x==desx-1 && n.y==desy-1 && n.d<answer){answer=n.d;}
+        int answer = -1;
+        n=maps.length;
+        m=maps[0].length;
+        PriorityQueue<Node> pq = new PriorityQueue<>((n1,n2)->n1.dis-n2.dis);
+        //출발 - 0,0 도착 n-1,m-1
+        pq.add(new Node(0,0,1));
+        boolean[][] visited = new boolean[n][m];
+        visited[0][0]=true;
+        while(!pq.isEmpty()){
+            Node nd = pq.poll();
+            if(nd.r==n-1 && nd.c==m-1){
+                answer = nd.dis;
+                break;
+            }
             for(int i=0; i<4; i++){
-                int nx = n.x+dx[i];
-                int ny = n.y+dy[i];
-                if(nx>=0 && ny>=0 && nx<desx && ny<desy){
-                    if(maps[ny][nx]==1&& !visited[ny][nx]){
-                        q.add(new Node(nx,ny,n.d+1));
-                        visited[ny][nx]=true;
-                    }
-                }
+                int nr = nd.r+dr[i];
+                int nc = nd.c+dc[i];
+                if(!inRange(nr,nc)) continue;
+                if(visited[nr][nc]) continue;
+                if(maps[nr][nc]==0) continue;
+                pq.add(new Node(nr,nc,nd.dis+1));
+                visited[nr][nc]=true;
             }
         }
-        if(answer==10000) return -1;
-        else{return answer;}
-        
+        return answer;
+    }
+    
+    public boolean inRange(int r, int c){
+        return r>=0&& c>=0 && r<n && c<m;
     }
 }
